7b122e6b55c7fc0d52e315bfecf7bd2afe9ed9e0
Spartan733 <gabrielbois2003@gmail.com>
2026-07-13 14:00:52 +0200

    Code product.js

--- GeniArtHub/front/js/product.js
+++ GeniArtHub/front/js/product.js
@@ -1,37 +1,70 @@
+const params = new URLSearchParams(window.location.search)
+const id = params.get("id");
+
 async function detail() {
     try {
-        const res =  await fetch ('http://localhost:3000/api/products/')
+        const res =  await fetch (`http://localhost:3000/api/products/${id}`)
         const data = await res.json();
 
         console.log(data)
 
-        const productContainer = document.querySelector(".detailoeuvre")
+        document.title = `${product.title}`;
 
-        let productHtml = "";
 
-        data.forEach(products => {
-            productHtml += `
-                <article>
-                    <img src="${products.image}" alt="${products.titre}">
-                    <h1>${products.titre}</h1>
-                    <p>${products.description}</p>
-                    <h2>Acheter pour :</h2>
-                    <span>${products.declinaison["prix"]}</span>
-                    <input type="number" id="quantity">
-                    <select name="format id="format"></select>
-                    <a href="#"
-                        <button>Acheter ${products.shorttitle}</buton>
-                    <a>
-                </article>
-                <aside>
-                    <h2>Description de l'oeuvre : ${products.title}</h2>
-                </aside>
-            `;
+        const img = document.querySelector("figure img");
+        img.src = product.image;
+        img.alt = product.title
+
+
+        const titre = document.querySelector("h1")
+        titre.textContent = product.title;
+
+
+        const descriptionCourte = document.querySelector("article div p")
+        descriptionCourte.textContent = product.description.substring(0, 150) + "..."
+
+
+        const descComplete = document.querySelector("aside")
+        descriptionComplete.innerHTML = `
+            <h2>Description de l'œuvre : ${product.title}</h2>
+            <p>${product.description}</p>
+        `;
+
+
+        // Recuperation du select
+        const select = document.querySelector("#format")
+
+        select.innerHTML = "";
+
+        // Creation des options du select
+        product.declinaison.forEach((declinaison) => {
+            const option = document.createElement("option");
+
+            option.value = declinaison.taille;
+
+            option.textContent = declinaison.taille;
+
+            select.appendChild(option)
+        })
+
+        const prix = document.querySelector(".showprice");
+
+        prix.textContent = product.declinaison[0].prix + "€";
+
+        select.addEventListener("change", () => {
+         const tailleChoisie = select.value;
+
+            const declinaisonChoisie = product.declinaisons.find(
+                (declinaison) =>
+                    declinaison.taille === tailleChoisie
+            );
+
+            prix.textContent = declinaisonChoisie.prix + "€";
         });
 
         productContainer.innerHTML = productHtml;
 
-    } catch {
-
+    } catch (error) {
+        console.log("Ereur lors de la récupération du produit", error);
     }
 }
